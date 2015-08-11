3024fe4e4481a24615c5c1cf2fa2dba84d370000
Michael Stephens <michael.stephens@biola.edu>
2015-08-11 13:52:51 -0700

    Escape everything that touches SQL

--- lib/trogdir_person.rb
+++ lib/trogdir_person.rb
@@ -6,30 +6,30 @@ class TrogdirPerson
   end
 
   def biola_id
-    get_value_from_nested_record(:ids, :biola_id, :identifier)
+    get_value_from_nested_record(:ids, :biola_id, :identifier).gsub("'", "\\\\'")
   end
 
   def legacy_biola_id
     zero_pad = Settings.biola_id_length - biola_id.length
-    "0"*zero_pad + biola_id
+    "0"*zero_pad + biola_id.gsub("'", "\\\\'")
   end
 
   def netid
-    get_value_from_nested_record(:ids, :netid, :identifier)
+    get_value_from_nested_record(:ids, :netid, :identifier).gsub("'", "\\\\'")
   end
 
   # Ex: johnd of netid johnd0
   def netid_name
-    netid.gsub(/[0-9]/, "")
+    netid.gsub(/[0-9]/, "").gsub("'", "\\\\'")
   end
 
   # Ex: 0 of netid johnd0
   def netid_number
-    netid.gsub(/[a-z]/, "")
+    netid.gsub(/[a-z]/, "").gsub("'", "\\\\'")
   end
 
   def name
-    "#{last_name}, #{first_name} #{middle_name}".gsub("'", "\\\\'")
+    "#{last_name}, #{first_name} #{middle_name}"
   end
 
   private
@@ -39,15 +39,15 @@ class TrogdirPerson
   end
 
   def first_name
-    hash['first_name']
+    hash['first_name'].gsub("'", "\\\\'")
   end
 
   def middle_name
-    hash['middle_name']
+    hash['middle_name'].gsub("'", "\\\\'")
   end
 
   def last_name
-    hash['last_name']
+    hash['last_name'].gsub("'", "\\\\'")
   end
 
   def get_value_from_nested_record(collection, type, return_attr)
